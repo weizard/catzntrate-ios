24d535249f5a44474d1166e1fa5b4e2b4a37a9da
weizard <purpledoor4921@gmail.com>
2022-02-19 18:08:46 -0700

    chore: remove usless code and add web3 package

--- Catzntrate/ContentView.swift
+++ Catzntrate/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import PopupView
+import web3
 
 let pet1 = Pet(id: 0, status: [10,20,4,95,98,0], attrs: [10,10,10,10], imageUrl: "bufficorn")
 let pet2 = Pet(id: 1, status: [1,0,0,100,100,1], attrs: [6,4,9,2], imageUrl: "cat")
@@ -17,15 +18,11 @@ extension View {
     }
 }
 
-
-
 struct ContentView: View {
     
     @State public var latestUsingPet = "0x" // latest user using pet's id
     @State public var pets:[Pet] = []
     @State public var currentPetIndex = 0 // current pet user use
-    
-    
     
     enum Tab {
         case CatWorking
@@ -35,8 +32,6 @@ struct ContentView: View {
     }
     
     var body: some View {
-       
-        
         TabView{
             CatWorkingView(pets: $pets, currentPetIndex: $currentPetIndex).tabItem{
                 Label("Work",systemImage: "text.bubble").tag(Tab.CatWorking)
